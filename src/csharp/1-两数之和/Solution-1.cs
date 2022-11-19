425d6e055367c42f0ff1caa926994662b904327c
b1acksoil <dev@sorabs.cc>
2022-11-20 03:47:52 +0800

    feat(csharp): add 1-1

---
+++ src/csharp/1-两数之和/Solution-1.cs
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=csharp
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        for (var i = 0; i < nums.Length; i++)
+        {
+            for (var j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                    return new[] { i, j };
+            }
+        }
+        
+        // no solution
+        return new[] { -1, -1 };
+    }
+}
+// @lc code=end
+
