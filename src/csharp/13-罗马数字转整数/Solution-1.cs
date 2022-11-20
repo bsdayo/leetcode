484a4ff655507bd0d4b44e9476821ea9e5632490
b1acksoil <dev@sorabs.cc>
2022-11-20 15:10:00 +0800

    feat(csharp): add 13-1

---
+++ src/csharp/13-罗马数字转整数/Solution-1.cs
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=13 lang=csharp
+ *
+ * [13] 罗马数字转整数
+ */
+
+namespace LeetCode.P13.S1;
+
+// @lc code=start
+public class Solution
+{
+    public int RomanToInt(string s)
+    {
+        var result = 0;
+        var last = 0;
+
+        foreach (var c in s)
+        {
+            var value = GetValue(c);
+            result += value;
+            if (value > last) result -= last * 2;
+            last = value;
+        }
+
+        return result;
+    }
+
+    public int GetValue(char c)
+    {
+        return c switch
+        {
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+}
+// @lc code=end
